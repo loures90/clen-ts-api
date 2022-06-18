f2a47b8d5f890fe82c917d05959e9a4a6d214405
loures90 <fernando.loures@hotmail.com>
2022-06-18 11:03:38 -0300

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*ts'],
-  coverageDirectory: "coverage",
+  collectCoverageFrom: ['<rootDir>/src/**/*ts', '!<rootDir>/src/**/*protocols.ts', '!<rootDir>/src/**/protocols/*ts'],
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
-};
+  },
+  testMatch: ['**/*.spec.ts', '**/*.test.ts']
+}
