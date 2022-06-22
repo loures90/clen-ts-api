6e07a7651f044d4ea50280a0156fbab503a5ddc0
loures90 <fernando.loures@hotmail.com>
2022-06-22 09:19:24 -0300

    test: ensure Account repository add method returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,24 @@
+import mongoHelper from '../helpers/mongo-helper'
+import { AccountRepository } from './account'
+
+describe('Account MongoRepository', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL)
+  })
+  test('Should call add and return an account on success', async () => {
+    const sut = new AccountRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
+  })
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+})
