f628ed46d7be4b349a367f8ad7b352c53988bfa7
loures90 <fernando.loures@hotmail.com>
2022-06-27 15:02:19 -0300

    test: ensure mongodb reconnect if client is down

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,18 @@
+import MongoHelper from './mongo-helper'
+
+describe('Mongo Helper', () => {
+  const sut = MongoHelper
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+  test('Should restart the connection when it fails', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
