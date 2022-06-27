1c2a7b8d81f90a61a58212109512255be428a316
loures90 <fernando.loures@hotmail.com>
2022-06-27 08:41:52 -0300

    test: ensure mongodb connects before test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest'
+import mongoHelper from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Signup routes', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL)
+  })
+  beforeEach(async () => {
+    const accountCollection = await mongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
