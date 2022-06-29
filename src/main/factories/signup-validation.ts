ba5c6ca26674332ac3dad8de38f848fcb73a7bd8
loures90 <fernando.loures@hotmail.com>
2022-06-29 15:58:52 -0300

    feat: ensure SignupValidation validates EmailValidation

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -2,6 +2,8 @@ import { ValidationComposite } from '../../presentation/helpers/validation/valid
 import { RequiredFieldsValidation } from '../../presentation/helpers/validation/required-fields-validation'
 import { Validation } from '../../presentation/helpers/validation/validation'
 import { CompareFieldsValidation } from '../../presentation/helpers/validation/compare-fields-validation'
+import { EmailValidation } from '../../presentation/helpers/validation/email-validation'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
 export const makeSignupValidation = (): ValidationComposite => {
   const validations: Validation[] = []
@@ -9,5 +11,6 @@ export const makeSignupValidation = (): ValidationComposite => {
     validations.push(new RequiredFieldsValidation(field))
   }
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
   return new ValidationComposite(validations)
 }
