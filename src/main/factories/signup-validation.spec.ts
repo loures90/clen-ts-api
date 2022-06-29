5bbcd105a788ace844f177c0e7d577bc88ce386e
loures90 <fernando.loures@hotmail.com>
2022-06-29 15:25:40 -0300

    test: ensure SignupValidation validates ComparedFieldsValidation

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validation/compare-fields-validation'
 import { RequiredFieldsValidation } from '../../presentation/helpers/validation/required-fields-validation'
 import { Validation } from '../../presentation/helpers/validation/validation'
 import { ValidationComposite } from '../../presentation/helpers/validation/validation-composite'
@@ -12,6 +13,7 @@ describe('SignupValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldsValidation(field))
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
