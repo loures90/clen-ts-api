cfaa49b20a3e7e942579d62eaa8c03ebcc672380
loures90 <fernando.loures@hotmail.com>
2022-06-29 17:53:52 -0300

    test: ensure ValidationComposite returns the correct error

--- src/presentation/helpers/validation/validation-composite.spec.ts
+++ src/presentation/helpers/validation/validation-composite.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
@@ -15,7 +16,7 @@ interface SutTypes {
   ValidationStubs: Validation[]
 }
 const makeSut = (): SutTypes => {
-  const ValidationStubs = [makeValidationStub()]
+  const ValidationStubs = [makeValidationStub(), makeValidationStub()]
   const sut = new ValidationComposite(ValidationStubs)
   return {
     sut,
@@ -29,5 +30,21 @@ describe('ValidationComposite', () => {
     const validateSpy = jest.spyOn(ValidationStubs[0], 'validate')
     sut.validate('any_value')
     expect(validateSpy).toHaveBeenCalledWith('any_value')
+  })
+
+  test('Should return the correct error if one of the validation fails', () => {
+    const { sut, ValidationStubs } = makeSut()
+    jest.spyOn(ValidationStubs[0], 'validate').mockReturnValueOnce(null)
+    jest.spyOn(ValidationStubs[1], 'validate').mockReturnValueOnce(new InvalidParamError('any_field'))
+    const error = sut.validate('any_value')
+    expect(error).toEqual(new InvalidParamError('any_field'))
+  })
+
+  test('Should return the correct error if more than one of the validation fails', () => {
+    const { sut, ValidationStubs } = makeSut()
+    jest.spyOn(ValidationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(ValidationStubs[1], 'validate').mockReturnValueOnce(new InvalidParamError('any_field'))
+    const error = sut.validate('any_value')
+    expect(error).toEqual(new Error())
   })
 })
