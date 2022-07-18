1cf1fa2c55d5effeb6b408a0fa8b74a6d95feac6
loures90 <fernando.loures@hotmail.com>
2022-07-18 11:32:34 -0300

    chore: update envs

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: 5050,
-  jwtSecret: 'tert78FD94.;/',
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'tert78FD94.;/',
   salt: 12
 }
