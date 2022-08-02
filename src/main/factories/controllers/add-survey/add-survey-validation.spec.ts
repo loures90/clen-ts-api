7e74400ff2ef3cbb797615254fbd3dc1c0b72b8a
loures90 <fernando.loures@hotmail.com>
2022-08-02 07:38:18 -0300

    test: ensure AddSurveyValidation calls correct validations

---
+++ src/main/factories/controllers/add-survey/add-survey-validation.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite, RequiredFieldsValidation } from '../../../../validation/validation'
+import { Validation } from '../../../../presentation/protocols'
+import { makeAddSurveyValidation } from './add-survey-validation'
+
+jest.mock('../../../../validation/validation/validation-composite')
+
+describe('LoginValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldsValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
