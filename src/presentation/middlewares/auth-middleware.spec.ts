144d0e455a404d764f1e193b5c4b1486b7417ccb
loures90 <fernando.loures@hotmail.com>
2022-08-02 23:12:55 -0300

    test: ensure AuthMiddleware returns 403 if x-access-token is not provided

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,48 @@
+import { AccountModel } from '../../domain/model/account'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccessDeniedError } from '../errors/access-denied-error'
+import { forbidden } from '../helpers/http/http-helpers'
+import { AuthMiddleware } from './auth-middleware'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@email.com',
+  password: 'hashed_password'
+})
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (token: string): Promise<AccountModel> {
+      return await new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+
+interface SutTypes {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthMiddleware()
+  return {
+    sut,
+    loadAccountByTokenStub
+  }
+}
+
+describe('AuthMiddleware', () => {
+  test('Should return 403 if x-access-token is not provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+  test('Should call loadAccountByToken with correct values', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+})
