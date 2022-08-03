91142b60b1831839cfc426ed2f27906196ce08a3
loures90 <fernando.loures@hotmail.com>
2022-08-03 19:25:03 -0300

    feat: ensure addSurveyRoute return 403 when no header is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,23 +2,66 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import mongoHelper from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import jsonwebtoken from 'jsonwebtoken'
+import config from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 describe('AddSurvey routes', () => {
   beforeAll(async () => {
     await mongoHelper.connect(process.env.MONGO_URL)
   })
   beforeEach(async () => {
     surveyCollection = await mongoHelper.getCollection('surveys')
+    accountCollection = await mongoHelper.getCollection('accounts')
   })
   afterEach(async () => {
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
   afterAll(async () => {
     await mongoHelper.disconnect()
   })
   describe('POST / addSurvey', () => {
-    test('Should return 204 on success', async () => {
+    test('Should return 204 on success to admin user', async () => {
+      const newAccount = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        role: 'any_role'
+      })
+      const id = newAccount.insertedId
+      const accessToken = await jsonwebtoken.sign({ id }, config.jwtSecret)
+      await accountCollection.findOneAndUpdate(
+        { _id: id },
+        { $set: { accessToken } })
+      await request(app)
+        .post('/api/survey')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            image: 'any_answer',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }
+          ]
+        })
+        .expect(204)
+    })
+    test('Should return 403 when no headers is provided', async () => {
+      const newAccount = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        role: 'any_role'
+      })
+      const id = newAccount.insertedId
+      const accessToken = await jsonwebtoken.sign({ id }, config.jwtSecret)
+      await accountCollection.findOneAndUpdate(
+        { _id: id },
+        { $set: { accessToken } })
       await request(app)
         .post('/api/survey')
         .send({
@@ -31,7 +74,7 @@ describe('AddSurvey routes', () => {
           }
           ]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
