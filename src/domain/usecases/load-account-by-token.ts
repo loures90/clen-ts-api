f308138760aa2703b82855caa9eb7329a4ff0749
loures90 <fernando.loures@hotmail.com>
2022-08-02 23:25:54 -0300

    feat: ensure AuthMiddleware calls loadAccountByToken with correct values

--- src/domain/usecases/load-account-by-token.ts
+++ src/domain/usecases/load-account-by-token.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../model/account'
 
 export interface LoadAccountByToken {
-  load (token: string): Promise<AccountModel>
+  load (token: string, role?: string): Promise<AccountModel>
 }
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,19 @@
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden } from '../helpers/http/http-helpers'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role: string
+  ) { }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.header['x-access-token']
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken, this.role)
+    }
     return await new Promise(resolve => resolve(forbidden(new AccessDeniedError())))
   }
 }
