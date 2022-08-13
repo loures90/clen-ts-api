26fe82781a6317f6713624d1d647a904b0021013
loures90 <fernando.loures@hotmail.com>
2022-08-13 13:19:30 -0300

    feat: ensure SurveyRepository loadAll returns a list of surveys

--- src/infra/db/mongodb/survey-repository/survey.ts
+++ src/infra/db/mongodb/survey-repository/survey.ts
@@ -1,10 +1,18 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/surveys/load-surveys-repository'
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { SurveyModel } from '../../../../domain/model/survey'
 import mongoHelper from '../helpers/mongo-helper'
 
-export class SurveyRepository implements AddSurveyRepository {
+export class SurveyRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await mongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
     return null
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await mongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys.map(survey => mongoHelper.mapper(survey))
   }
 }
