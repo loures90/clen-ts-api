dac022a043d54bd8a4da7ccc550d0220e746a467
loures90 <fernando.loures@hotmail.com>
2022-08-19 17:16:00 -0300

    test: ensure LoaSurvey Repository loadById returns an survey on success

--- src/infra/db/mongodb/survey-repository/survey.spec.ts
+++ src/infra/db/mongodb/survey-repository/survey.spec.ts
@@ -23,51 +23,78 @@ describe('Survey MongoRepository', () => {
   const makeSut = (): SurveyRepository => {
     return new SurveyRepository()
   }
-  test('Should call add and return null on success', async () => {
-    const sut = makeSut()
-    const surveyData = {
-      question: 'any_question',
-      answers: [{
-        image: 'any_answer',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }],
-      date: new Date()
-    }
-    await sut.add(surveyData)
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    const surveyResponse = mongoHelper.mapper(survey)
-    expect(surveyResponse).toBeTruthy()
-    expect(surveyResponse.id).toBeTruthy()
-    expect(surveyResponse.question).toBe('any_question')
-    expect(surveyResponse.answers.length).toBe(2)
+  describe('add', () => {
+    test('Should call add and return null on success', async () => {
+      const sut = makeSut()
+      const surveyData = {
+        question: 'any_question',
+        answers: [{
+          image: 'any_answer',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }
+      await sut.add(surveyData)
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      const surveyResponse = mongoHelper.mapper(survey)
+      expect(surveyResponse).toBeTruthy()
+      expect(surveyResponse.id).toBeTruthy()
+      expect(surveyResponse.question).toBe('any_question')
+      expect(surveyResponse.answers.length).toBe(2)
+    })
+  })
+  describe('LoadAll', () => {
+    test('Should call LoadAll and return a list of surveys on success', async () => {
+      const sut = makeSut()
+      const surveyData = {
+        question: 'any_question',
+        answers: [{
+          image: 'any_answer',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }
+      await surveyCollection.insertOne(surveyData)
+      const surveys = await sut.loadAll()
+      expect(surveys).toBeTruthy()
+      expect(surveys[0].id).toBeTruthy()
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[0].answers.length).toBe(2)
+      expect(surveys[0].date).toBeTruthy()
+    })
+    test('Should call LoadAll and return an empty list when no survey is found', async () => {
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys).toBeTruthy()
+      expect(surveys.length).toBe(0)
+    })
   })
 
-  test('Should call LoadAll and return a list of surveys on success', async () => {
-    const sut = makeSut()
-    const surveyData = {
-      question: 'any_question',
-      answers: [{
-        image: 'any_answer',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }],
-      date: new Date()
-    }
-    await surveyCollection.insertOne(surveyData)
-    const surveys = await sut.loadAll()
-    expect(surveys).toBeTruthy()
-    expect(surveys[0].id).toBeTruthy()
-    expect(surveys[0].question).toBe('any_question')
-    expect(surveys[0].answers.length).toBe(2)
-    expect(surveys[0].date).toBeTruthy()
-  })
-  test('Should call LoadAll and return an empty list when no survey is found', async () => {
-    const sut = makeSut()
-    const surveys = await sut.loadAll()
-    expect(surveys).toBeTruthy()
-    expect(surveys.length).toBe(0)
+  describe('LoadById', () => {
+    test('Should call LoadById and return a survey on success', async () => {
+      const surveyData = {
+        question: 'any_question',
+        answers: [{
+          image: 'any_answer',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }
+      await surveyCollection.insertOne(surveyData)
+      const survey = mongoHelper.mapper(surveyData)
+      const sut = makeSut()
+      const surveyResponse = await sut.loadById(survey.id)
+
+      expect(surveyResponse).toBeTruthy()
+      expect(surveyResponse.id).toBeTruthy()
+      expect(surveyResponse.question).toBe('any_question')
+      expect(surveyResponse.answers.length).toBe(2)
+    })
   })
 })
