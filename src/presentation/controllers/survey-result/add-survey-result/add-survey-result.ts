941108839498a63e7c14775469bb4833e71442c0
loures90 <fernando.loures@hotmail.com>
2022-08-26 11:02:04 -0300

    feat: ensure addSurveyController return 403 on invalid surveyId

--- src/presentation/controllers/survey-result/add-survey-result/add-survey-result.ts
+++ src/presentation/controllers/survey-result/add-survey-result/add-survey-result.ts
@@ -1,10 +1,15 @@
+import { InvalidParamError } from '../../../errors'
+import { forbidden } from '../../../helpers/http/http-helpers'
 import { LoadSurveyById, Controller, HttpRequest, HttpResponse } from './protocols'
 
 export class AddSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.survey_id)
+    const survey = await this.loadSurveyById.loadById(httpRequest.params.survey_id)
+    if (!survey) {
+      return forbidden(new InvalidParamError('survey_id'))
+    }
     return null
   }
 }
