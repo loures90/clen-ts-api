4791b7d67e4c73a3d22ba53a144e5d3ccef01063
loures90 <fernando.loures@hotmail.com>
2022-09-05 10:14:58 -0300

    docs: setup swagger

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import middlewares from './middlewares'
 import setupRoutes from './routes'
+import configSwagger from './swagger'
 
 const app = express()
+configSwagger(app)
 middlewares(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Api de Enquetes para programadores',
+    description: 'Curso Manguinho',
+    version: '1.0.0'
+  }
+}
