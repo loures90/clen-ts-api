fc3a21196b7d6a987f63af2751699e6838a40b66
loures90 <fernando.loures@hotmail.com>
2022-09-09 18:54:11 -0300

    feat: ensure loadSurveyResultController calls loadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../../domain/usecases/load-survey-by-id'
+export * from '../../../protocols'
+export * from '../../../helpers/http/http-helpers'
+export * from '../../../errors/invalid-param-error'
