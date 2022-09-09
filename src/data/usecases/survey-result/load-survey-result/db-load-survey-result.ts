5b0d0967f50afcbb76a8ac545f29ed28ca1062c2
loures90 <fernando.loures@hotmail.com>
2022-09-09 16:07:19 -0300

    feat: ensure DbLoadSurveyResult calls loadSurveyByIdRespository with correct surveyId

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,11 +1,17 @@
 import { SurveyResultModel } from '../add-survey-result/db-add-survey-result-protocols'
-import { LoadSurveyResult, LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+import { LoadSurveyResult, LoadSurveyResultRepository, LoadSurveyByIdRepository } from './db-load-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) { }
 
   async load (surveyId: string): Promise<SurveyResultModel> {
-    await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId)
+    }
     return await new Promise(resolve => resolve(null))
   }
 }
