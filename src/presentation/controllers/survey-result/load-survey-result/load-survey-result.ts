2fe4a17513abefd78b8f7ac145d686737c74e68b
loures90 <fernando.loures@hotmail.com>
2022-09-09 19:01:29 -0300

    feat: ensure loadSurveyResultController returns 403 to invalid survey id

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result.ts
@@ -1,11 +1,14 @@
-import { HttpRequest, HttpResponse, Controller, LoadSurveyById } from './protocols'
+import { HttpRequest, HttpResponse, Controller, LoadSurveyById, forbidden, InvalidParamError } from './protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { surveyId } = httpRequest.params
-    await this.loadSurveyById.loadById(surveyId)
+    const isSurveyValid = await this.loadSurveyById.loadById(surveyId)
+    if (!isSurveyValid) {
+      return forbidden(new InvalidParamError('survey_id'))
+    }
     return await new Promise(resolve => resolve(null))
   }
 }
