f79f0ab713ef6bff324458ce9b6bef0f2f9a2b90
loures90 <fernando.loures@hotmail.com>
2022-10-25 09:01:51 -0300

    feat: ensure static files are called correctly

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import middlewares from './middlewares'
 import setupRoutes from './routes'
 import configSwagger from './swagger'
+import staticFiles from './static'
 
 const app = express()
+staticFiles(app)
 configSwagger(app)
 middlewares(app)
 setupRoutes(app)
